d36d46daa3b466a89607ce4ace094941bf1c37ec
MuhIlham420 <muh.ilhammbo@gmail.com>
2025-12-07 23:16:30 +0700

    hapus yang gaperlu

--- backup-script/config.sh
+++ backup-script/config.sh
@@ -3,12 +3,6 @@
 # Lokasi file log
 LOG_FILE="..\log\backup.log"
 
-# Direktori sumber untuk backup
-SOURCE_DIR="..\sample-file"
-
-# Direktori tujuan untuk menyimpan backup
-BACKUP_DIR="..\backup-file"
-
 # Format nama file backup
 DATE_FORMAT=$(date +%Y%m%d-%H%M%S)
 BACKUP_NAME="backup-$DATE_FORMAT.tar.gz"
